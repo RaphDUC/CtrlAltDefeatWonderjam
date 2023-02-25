8233cc5e71d82c152ac804abe80292e922bbb201
Maxime Morin <maxime_morette@hotmail.com>
2023-02-25 03:52:13 -0500

    Repaired Project

---
+++ Source/WonderjamH23/WonderjamH23.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WonderjamH23.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WonderjamH23, "WonderjamH23" );
---
+++ Source/WonderjamH23/WonderjamH23GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WonderjamH23GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WONDERJAMH23_API AWonderjamH23GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
